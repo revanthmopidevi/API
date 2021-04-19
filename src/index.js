6bae7d7367b717ebdae9a4758a8968248e75d954
Revanth Mopidevi <39304828+revanthmopidevi@users.noreply.github.com>
2021-04-19 12:38:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
     res.status(200).send(`Sample API for SecureX. Number of hits = ${hits}`)
 })
 
+app.get('/status', (req, res) => {
+    res.send(`current hits = ${hits}`)
+})
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
