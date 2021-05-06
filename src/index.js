745f811f20a513eb807baa3ae98c3dd0baa5e894
Revanth Mopidevi <39304828+revanthmopidevi@users.noreply.github.com>
2021-05-06 12:24:14 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ const app = express()
 const port = process.env.PORT || 3000
 
 let hits = 0
+let data = [
+    {"month":"January","revenue":2025},
+    {"month":"Feb","revenue":1882},
+    {"month":"March","revenue":1859},
+    {"month":"April","revenue":2022},
+    {"month":"May","revenue":2021},
+    {"month":"June","revenue":2019},
+    {"month":"July","revenue":2008},
+    {"month":"August","revenue":2007},
+    {"month":"September","revenue":1997},
+    {"month":"October","revenue":2000},
+    {"month":"November","revenue":2025},
+    {"month":"December","revenue":2025}
+]
 
 app.get('/', (req, res) => {
     hits += 1
@@ -12,6 +26,10 @@ app.get('/', (req, res) => {
 
 app.get('/status', (req, res) => {
     res.send(`current hits = ${hits}`)
+})
+
+app.get('/data', (req, res)=> {
+    res.status(200).send(data)
 })
 
 
