3a781a8ac4dc20b7b4a5a80e3ae137e787b5d257
Revanth Mopidevi <39304828+revanthmopidevi@users.noreply.github.com>
2021-05-13 18:39:19 +0530

    Update V2.js

--- src/routers/V2.js
+++ src/routers/V2.js
@@ -6,10 +6,10 @@ const router = new express.Router()
 // POST alert
 router.post('/alert', async (req, res) => {
     const toPersonEmail = "revanthmopidevi@gmail.com"
-    const Alerts = request.body.Alerts
+    const alerts = request.body.Alerts
     // Alert Status, End Date, Start Date
     try {
-        await sendMessage(Alerts, toPersonEmail)
+        await sendMessage(alerts, toPersonEmail)
         res.status(200).send({
             "text": req.body.Alerts,
             "email": toPersonEmail
