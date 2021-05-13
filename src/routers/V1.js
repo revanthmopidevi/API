e8747efed91204bb1cae24929d1d47408087584c
Revanth Mopidevi <revanthmopidevi@.com>
2021-05-13 09:41:29 +0530

    routes

--- src/routers/V1.js
+++ src/routers/V1.js
@@ -2,15 +2,40 @@ const express = require('express')
 
 const router = new express.Router()
 
+// initialised to 0 hits
 let hits = 0
 
+// hard-coded data
+let data = [
+    {"month":"January","revenue":2025},
+    {"month":"Feb","revenue":1882},
+    {"month":"March","revenue":1859},
+    {"month":"April","revenue":2022},
+    {"month":"May","revenue":2021},
+    {"month":"June","revenue":2019},
+    {"month":"July","revenue":2008},
+    {"month":"August","revenue":2007},
+    {"month":"September","revenue":1997},
+    {"month":"October","revenue":2000},
+    {"month":"November","revenue":2025},
+    {"month":"December","revenue":2025}
+]
+
+// GET hit
 router.get('/', (req, res) => {
     hits += 1
     res.status(200).send(`Sample API for SecureX. Number of hits = ${hits}`)
 })
 
+// GET hits count
 router.get('/hits', (req, res) => {
     res.send(`current hits = ${hits}`)
 })
+
+// GET data
+router.get('/data', (req, res)=> {
+    res.status(200).send(data)
+})
+
 
 module.exports = router
--- src/routers/V2.js
+++ src/routers/V2.js
@@ -3,30 +3,12 @@ const sendMessage = require('../modules/webex')
 
 const router = new express.Router()
 
-let data = [
-    {"month":"January","revenue":2025},
-    {"month":"Feb","revenue":1882},
-    {"month":"March","revenue":1859},
-    {"month":"April","revenue":2022},
-    {"month":"May","revenue":2021},
-    {"month":"June","revenue":2019},
-    {"month":"July","revenue":2008},
-    {"month":"August","revenue":2007},
-    {"month":"September","revenue":1997},
-    {"month":"October","revenue":2000},
-    {"month":"November","revenue":2025},
-    {"month":"December","revenue":2025}
-]
-
-router.get('/data', (req, res)=> {
-    res.status(200).send(data)
-})
-
+// POST alert
 router.post('/alert', async (req, res) => {
     try {
         await sendMessage(req.body.text, req.body.toPersonEmail)
         res.status(200).send()
-    } catch (e) {
+    } catch(e) {
         res.status(500).send({
             "error": `Message Not Delivered: ${e}`
         })
