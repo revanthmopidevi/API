52e3a79783a22b0cef5face4ee5765517971f7ce
Revanth Mopidevi <39304828+revanthmopidevi@users.noreply.github.com>
2021-05-17 10:10:51 +0530

    Update webex.js

--- src/modules/webex.js
+++ src/modules/webex.js
@@ -1,5 +1,5 @@
 const Webex = require(`webex`)
-const TOKEN = "ZjM4MDg1YzQtOWJlNS00ODk1LWFjOTUtMTYwOGMxYWJkNTJhZTA2MjZhY2QtZjRj_PF84_93291e23-3b2a-45e1-b496-b695b04d0daa"
+const TOKEN = "NjNmMTUxNjgtNjYwNi00MjZmLTg4OTMtNDgyOWExN2YwNzcwNDY3MTk3MzItYjI0_PF84_93291e23-3b2a-45e1-b496-b695b04d0daa"
 const webex = Webex.init({
   credentials: {
     access_token: TOKEN
